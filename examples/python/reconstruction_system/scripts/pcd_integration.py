1e132b0e53c85045e79549024a94b67b81b62e3d
shilem2 <moshe.shilemay@medtronic.com>
2024-05-10 00:05:07 +0300

    icp playground

--- examples/python/reconstruction_system/scripts/pcd_integration.py
+++ examples/python/reconstruction_system/scripts/pcd_integration.py
@@ -494,13 +494,16 @@ def icp_playground():
 
     voxel_size = 0.01
 
-    source = copy.deepcopy(pcd1).voxel_down_sample(voxel_size=voxel_size)
-    target = copy.deepcopy(pcd0).voxel_down_sample(voxel_size=voxel_size)
-    trans_init = T
+    # source = copy.deepcopy(pcd1).voxel_down_sample(voxel_size=voxel_size)
+    # target = copy.deepcopy(pcd0).voxel_down_sample(voxel_size=voxel_size)
+    source = copy.deepcopy(pcd1)
+    target = copy.deepcopy(pcd0)
+    # trans_init = T
+    trans_init = np.identity(4)
 
     draw_registration_result(source, target, trans_init)
 
-    threshold = 500
+    threshold = 0.005
 
     print("Initial alignment")
     evaluation = o3d.pipelines.registration.evaluate_registration(source, target, threshold, trans_init)
@@ -531,14 +534,15 @@ def icp_playground():
 def draw_registration_result(source, target, transformation):
     source_temp = copy.deepcopy(source)
     target_temp = copy.deepcopy(target)
-    source_temp.paint_uniform_color([1, 0.706, 0])
-    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    # source_temp.paint_uniform_color([1, 0.706, 0])
+    # target_temp.paint_uniform_color([0, 0.651, 0.929])
     source_temp.transform(transformation)
     o3d.visualization.draw_geometries([source_temp, target_temp],
-                                      zoom=0.4459,
-                                      front=[0.9288, -0.2951, -0.2242],
-                                      lookat=[1.6784, 2.0612, 1.4451],
-                                      up=[-0.3402, -0.9189, -0.1996])
+                                      # zoom=0.4459,
+                                      # front=[0.9288, -0.2951, -0.2242],
+                                      # lookat=[1.6784, 2.0612, 1.4451],
+                                      # up=[-0.3402, -0.9189, -0.1996],
+                                      )
 
 
 def align_rgb_depth_example():
@@ -728,10 +732,10 @@ def align_rgb_to_depth(rgb, intrinsics_rgb, depth, intrinsics_depth, depth_scale
 if __name__ == '__main__':
 
     # pcd_integration()
-    pcd_integration_IQ_meshes()
+    # pcd_integration_IQ_meshes()
     # pcd_registration()
     # run_reconstruction_system()
-    # icp_playground()
+    icp_playground()
     # align_rgb_depth_example()
 
 
